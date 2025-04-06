c5a84f41d50a6ca72b8a0cc5d32ad33583afe561
Sang Do <dovansang97@gmail.com>
2025-04-06 23:39:39 +0700

    Refactor font imports and update layout to include Bricolage Grotesque font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,16 +1,26 @@
-import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import {
+  Bricolage_Grotesque,
+  Geist,
+  Geist_Mono,
+  Inter,
+} from 'next/font/google';
 
 export const inter = Inter({
-    variable: '--font-inter',
-    subsets: ['latin'],
-})
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
 
 export const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-})
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 
 export const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-})
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const bricolageGrotesque = Bricolage_Grotesque({
+  variable: '--font-bricolage-grotesque',
+  subsets: ['latin'],
+});
